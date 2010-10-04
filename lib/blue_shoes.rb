dec3720fd44c6f1c14577879468742aa8b0bf3cc
Steve Klabnik <steve@steveklabnik.com>
2010-10-04 18:36:20 -0400

    Requiring the whole directory.
    I've often seen people require entire directories explicitly, but that
    seems like it's prone to people forgetting to add new files in. So let's
    just require the whole directory. It's easier that way.

--- lib/blue_shoes.rb
+++ lib/blue_shoes.rb
@@ -1,38 +1,20 @@
+#yay qt4!
 require 'Qt4'
 
-#this should just have some sort of require_directory call
-#yay vim macros!
-require_relative 'blue_shoes/app'
-require_relative 'blue_shoes/canvas'
-require_relative 'blue_shoes/color'
-require_relative 'blue_shoes/dialog'
-require_relative 'blue_shoes/download'
-require_relative 'blue_shoes/effect'
-require_relative 'blue_shoes/exception'
-require_relative 'blue_shoes/flow'
-require_relative 'blue_shoes/image'
-require_relative 'blue_shoes/link_url'
-require_relative 'blue_shoes/mask'
-require_relative 'blue_shoes/mouse'
-require_relative 'blue_shoes/native'
-require_relative 'blue_shoes/pattern'
-require_relative 'blue_shoes/search'
-require_relative 'blue_shoes/shape'
-require_relative 'blue_shoes/stack'
-require_relative 'blue_shoes/text'
-require_relative 'blue_shoes/text_block'
-require_relative 'blue_shoes/timer_base'
-require_relative 'blue_shoes/video'
-require_relative 'blue_shoes/widget'
-require_relative 'blue_shoes/window'
-
+#we want to require everything in the blue_shoes directory.
+Dir.glob("#{File.expand_path(File.dirname(__FILE__))}/blue_shoes/*.rb").each do |f|
+  require_relative f
+end
 
 module Shoes
 
+  #this is the code name for this release of Shoes.
   RELEASE_NAME = "Suede"
 
+  #this is the ID, I haven't decided how to generate them yet.
   RELEASE_ID = 1337
 
+  #this is the specific git revision. Haven't decided if I'm keeping it.
   REVISION = "e3dce7d0b5c2a45f67b2a0d453aa8c31ecd4422e"
 
   # a list of fonts
@@ -46,5 +28,4 @@ module Shoes
   end
 
 end
-
 
